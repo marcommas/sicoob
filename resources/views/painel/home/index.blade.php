586a7a613094ad1d1a6dac2602c9c17d99c8d1b2
Marco Comassetto <marcommas@gmail.com>
2016-03-22 09:00:01 -0300

    Versão 1.1  com carousel de imagens na home

--- resources/views/painel/home/index.blade.php
+++ resources/views/painel/home/index.blade.php
@@ -2,5 +2,22 @@
 
 @section('content')
 
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    </ol>
+
+    <div class="carousel-inner" role="listbox" >
+        <div class="item active">
+            <img src="{{url('assets/img/inicial1.jpg')}}" >
+        </div>
+        <div class="item">
+            <img src="{{url('assets/img/inicial2.jpg')}}" >
+        </div>
+    </div>
+</div>
+
 @endsection
 
