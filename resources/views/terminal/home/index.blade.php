4c69bc44b2662df3350df8bba7ec014bf4a718de
Marco Comassetto <marcommas@gmail.com>
2016-03-29 10:53:43 -0300

    Arrumado impressão de crônica

--- resources/views/terminal/home/index.blade.php
+++ resources/views/terminal/home/index.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php
+        //header("Content-Type: application/octet-stream"); 
+        //header('Content-Disposition: attachment; filename="1458737183-teste.pdf"');
+        //header('Content-Transfer-Encoding: binary');
+        // header('Content-Length: ' . filesize($file));
+        //header('Accept-Ranges: bytes');
+        ?>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
         <title>Terminal | Sicoob</title>
 
         <link rel="icon" type="image/png" href="{{url('assets/img/favicon.ico')}}">
-        
+
         <link rel="stylesheet" href="{{url('assets/css/bootstrap.css')}}" >
 
         <link rel="stylesheet" href="{{url('assets/css/bootstrap-theme.css')}}" >
@@ -19,9 +24,9 @@
 
     </head>
     <body style="background-image:url({{url('assets/img/background.jpg')}});  background-position: center;  background-size: 100% 100%;">
-     
 
-        <form class="form-padrao"  method="POST" action="/terminal/home/imprimir-cronica" send="/terminal/home/imprimir-cronica" >
+
+        <form id="formulario" class="form-padrao"  method="POST" action="/terminal/home/imprimir-cronica" send="/terminal/home/imprimir-cronica" >
             {!! csrf_field() !!}
             <div class="jumbotro vertical-center">
                 <input type="hidden" name="id_usuario" value="{{Auth::user()->id}}"  />
@@ -31,28 +36,19 @@
                     @foreach ($cronicas->all() as $cronica)
                     @if(  ($cronica->caminho_arquivo) != "" )
 
-                    <!--<button type="submit" id="btn-imprimir"  onclick="imprimir('iframeCronica');" class="btn btn-default btn-lg active" >{{$cronica->id }} - {{$cronica->cronica }} </button>
-                    -->
-                    
-                    <button type="submit" class="botao" onclick="imprimir('iframeCronica');" ><span class="botaoTextoSup">PRESSIONE AQUI</span><br><span class="botaoTextoInf">E RETIRE SEU TEXTO CULTURAL</span></button>
-                    
+                    <button type="button" class="botao" onclick="imprimir()" ><span class="botaoTextoSup">PRESSIONE AQUI</span><br><span class="botaoTextoInf">E RETIRE SEU TEXTO CULTURAL</span></button>
+
                     <input type="hidden" name="id_cronica" value="{{$cronica->id}}" />
-                    
+
                     <input type="hidden" name="posicao" value="{{$cronica->posicao}}" />
 
                     @endif
                     @endforeach
                     
-                   <!--
-                    <div id="aguardeImpressao" class="alert alert-success alert-dismissable" style="display:none;" >
-                        <h1>Aguarde a sua impressão...</h1>
-                    </div>
-                    -->
-                    <iframe id='iframeCronica' src='/assets/painel/upload/cronicas/{{$cronica->caminho_arquivo}}' style="display:none;"  ></iframe>
+
+                     <iframe id='iframeCronica' name='iframeCronica' src='/assets/painel/upload/cronicas/{{$cronica->caminho_arquivo}}' style="display:none;"  ></iframe>
 
                 </div>
-
-            </div>
 
         </form>
 
@@ -61,67 +57,19 @@
 
 
         <script type="text/javascript">
-
-            function imprimir(id) {
-                var x = document.getElementById(id);
-                x.contentWindow.print();
-            }
-           
-            
-            /*$(function () {
-                jQuery("form.form-padrao").submit(function () {
-
-                    jQuery(".msg-war").hide();
-                    jQuery(".aguardeImpressao").hide();
-
-                    var dadosForm = jQuery(this).serialize();
-
-                    jQuery.ajax({
-                        url: jQuery(this).attr("send"),
-                        data: dadosForm,
-                        type: "POST",
-                        beforeSend: iniciaPreloader()
-                    }).done(function (data) {
-                        finalizaPreloader();
-
-                        if (data == "1") {
-                            jQuery(".msg-suc").html("Aguarde a sua impressão...");
-                            jQuery(".msg-suc").show();
-
-                            //setTimeout("jQuery('.msg-suc').hide();jQuery('#modalGestao').modal('hide');location.reload();", 5000);
-                            setTimeout("jQuery('.msg-suc').hide();jQuery('#modalGestao').modal('hide');", 5000);
-                        } else {
-                            jQuery(".msg-war").html(data);
-                            jQuery(".msg-war").show();
-
-                            setTimeout("jQuery('.msg-war').hide();", 4500);
-                        }
-                    }).fail(function () {
-                        finalizaPreloader();
-                        alert("Falha Inesperada!");
-                    });
-
-                    return false;
-                });
-            });
- 
-            function iniciaPreloader() {
-                jQuery(".preloader").show();
-            }
-
-            function finalizaPreloader() {
-                jQuery(".preloader").hide();
-            }*/
-            
-            
-            /*function aguardeImpressao(){
-
-                document.getElementById("aguardeImpressao").style.display="block";
+                              
+            function imprimir() {
+                document.getElementById("iframeCronica").contentWindow.print();
                 
-                setTimeout(function(){
-                    document.getElementById("aguardeImpressao").style.display="none";
-                },4000);
-            }*/
+                setTimeout(
+                    function(){ 
+                        formulario.submit();
+                }, 5000);
+            }
+
+
+                         
+
         </script>
 
     </body>
